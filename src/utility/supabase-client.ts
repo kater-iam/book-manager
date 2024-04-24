c23a8c7a6fe380556d394664bdffb8990ae095cb
iwasaki <s.iwasaki1112@gmail.com>
2024-04-24 23:17:52 +0900

    - Updated configure file.

--- src/utility/supabase-client.ts
+++ src/utility/supabase-client.ts
@@ -1,8 +1,9 @@
 import { createClient } from "@refinedev/supabase";
 
-const SUPABASE_URL = "https://iwdfzvfqbtokqetmbmbp.supabase.co";
+const SUPABASE_URL = "https://rketssdpmjzyfglufthh.supabase.co";
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrZXRzc2RwbWp6eWZnbHVmdGhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5NjUyODUsImV4cCI6MjAyOTU0MTI4NX0.1FxBn7pBHmxmIgU_jTPzqcEvHfRkszuLajuJErJ6xd4";
+
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {
